d57c26286199ae03310c2c6fe5a11a2bc16be54e
Padraig Gleeson <p.gleeson@gmail.com>
2015-04-09 13:44:24 +0100

    Initial version in NeuroML2

---
+++ NEURON/export_neuroml2.py
@@ -0,0 +1,8 @@
+from pyneuroml.neuron import export_to_neuroml2
+
+export_to_neuroml2("091008A2.hoc", "../NeuroML2/dLGN.cell.nml", includeBiophysicalProperties=False)
+
+#export_to_neuroml2("test.hoc", "test.biophys.nml", includeBiophysicalProperties=True)
+
+
+
